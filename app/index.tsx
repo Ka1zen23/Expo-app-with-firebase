0b31c92f2267c605e1c0ad57e2485c60a026bbc2
seallover23 <122201770+Ka1zen23@users.noreply.github.com>
2025-09-22 18:16:21 +0800

    added authentication handling here instead of layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,57 @@
 // app/index.tsx
 import AnimatedIntro from "@/components/AnimatedIntro";
 import LoginSheet from "@/components/BottomLoginSheet";
-import { View } from "react-native";
+import { FIREBASE_AUTH } from '@/FirebaseConfig';
+import { router } from 'expo-router';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
 
 export default function Index() {
-return (
-    <View
-      style={{
-        flex: 1,
-      }}
-    >
-      <AnimatedIntro />
-      <LoginSheet />
-    </View>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      setIsLoading(false);
+      if (user) {
+        setIsAuthenticated(true);
+        router.replace('/(drawer)/(tabs)');
+      } else {
+        setIsAuthenticated(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <View style={styles.container}>
+        <AnimatedIntro />
+        <LoginSheet />
+      </View>
+    );
+  }
+
+  return null;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
